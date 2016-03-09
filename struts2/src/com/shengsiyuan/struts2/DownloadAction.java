eb21040e7e8e49a82c2dda30dfc730299ee111c6
sundalei <sundalei2011@163.com>
2016-03-09 22:44:34 +0800

    File download example with struts2

---
+++ struts2/src/com/shengsiyuan/struts2/DownloadAction.java
@@ -0,0 +1,17 @@
+package com.shengsiyuan.struts2;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class DownloadAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+	
+}
